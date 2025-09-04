8290abb6dbdc570ae5f7d3b6b30e822e14a43f38
thomas <amiscremail@gmail.com>
2025-09-04 15:02:27 +0200

    added acl to bucket

--- infra/modules/hosting/main.tf
+++ infra/modules/hosting/main.tf
@@ -15,20 +15,22 @@ resource "aws_s3_bucket" "spa" {
   tags          = var.tags
   force_destroy = true
 }
+
+resource "aws_s3_bucket_acl" "spa_acl" {
+  bucket = aws_s3_bucket.spa.id
+  acl    = "private"
+}
+
 // Log bucket for CloudFront access logs
 resource "aws_s3_bucket" "log" {
   count  = var.enable_logging ? 1 : 0
   bucket = "${terraform.workspace}-${var.stack_id}-spa-logs-${random_id.log_bucket_suffix[0].hex}"
   tags = var.tags
 }
-// Ensure ownership controls allow ACLs (don't use BucketOwnerEnforced which disables ACLs)
-resource "aws_s3_bucket_ownership_controls" "log_ownership" {
-  count  = var.enable_logging && var.log_bucket_allow_acl ? 1 : 0
+resource "aws_s3_bucket_acl" "log_acl" {
+  count  = var.enable_logging ? 1 : 0
   bucket = aws_s3_bucket.log[0].id
-
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
+  acl    = "log-delivery-write"
 }
 # Enable versioning on the log bucket
 resource "aws_s3_bucket_versioning" "log" {
@@ -48,12 +50,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "log" {
       sse_algorithm = "AES256"
     }
   }
-}
-// Grant CloudFront log delivery group WRITE and READ_ACP permissions
-resource "aws_s3_bucket_acl" "log_acl" {
-  count  = var.enable_logging && var.log_bucket_allow_acl ? 1 : 0
-  bucket = aws_s3_bucket.log[0].id
-  acl    = "log-delivery-write"
 }
 
 resource "aws_cloudfront_origin_access_control" "spa_oac" {
@@ -95,7 +91,7 @@ resource "aws_cloudfront_distribution" "spa" {
   }
 
   dynamic "logging_config" {
-    for_each = var.enable_logging && var.log_bucket_allow_acl ? [1] : []
+    for_each = var.enable_logging ? [1] : []
     content {
       bucket          = aws_s3_bucket.log[0].bucket_regional_domain_name
       include_cookies = false
