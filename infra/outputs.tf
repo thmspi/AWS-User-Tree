be60fe4121cd729470e2fbcf6b59a48be3934288
thomas <amiscremail@gmail.com>
2025-09-09 10:09:11 +0200

    modified signature of the verification mail

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,15 +2,3 @@
 output "cloudfront_domain" {
   value = "https://${module.hosting.cloudfront_domain}"
 }
-
-output "user_pool_id" {
-  value = module.auth.user_pool_id
-}
-
-output "spa_client_id" {
-  value = module.auth.spa_client_id
-}
-
-output "dashboard_url" {
-  value = module.hosting.dashboard_url
-}
